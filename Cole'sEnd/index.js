f3185147d279a8e011030eca96007d9ad797d3b8
ColeStin <stinsonca16@gmail.com>
2020-02-08 02:41:28 -0600

    Contributing Photo Change software

---
+++ Cole'sEnd/index.js
@@ -0,0 +1,25 @@
+class Tile
+{
+    constructor(name)
+    {
+        this.blockNumber;
+        this.type = name;
+        console.log(name);
+    }
+    printName() 
+    {
+        console.log(name);
+        if(name = "human")
+        {
+            console.log("Changing photos");
+            var image = document.getElementById('block1');
+            console.log("Photo Location found");
+            image.src = "block1.png"
+        }
+    }
+    
+
+}
+var name = window.prompt("Enter: ");
+let block = new Tile(name); 
+block.printName();
